23cfba43e919cbd28051f2e4fcb2aa6730878dab
Tobias Renner <tobi.renner03@gmail.com>
2023-08-31 17:05:03 +0200

    Initial Commit

---
+++ kurswahl/src/main/java/com/ubi/kurswahl/KurswahlApplication.java
@@ -0,0 +1,13 @@
+package com.ubi.kurswahl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurswahlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurswahlApplication.class, args);
+	}
+
+}
---
+++ kurswahl/src/test/java/com/ubi/kurswahl/KurswahlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubi.kurswahl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurswahlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
