67b0a30db48fbe45b352178b86633c020f4cfafe
Tobias Renner <tobi.renner03@gmail.com>
2023-09-10 17:10:13 +0200

    feat: sidenav entry for admin

--- kurswahl-ui/src/app/component/common/sidenav/sidenav.component.ts
+++ kurswahl-ui/src/app/component/common/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from "../../../service/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidenav',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent {
+  constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
+  getRole(): string | null {
+    return this.authenticationService.getRole();
+  }
+
+  navigateTo(url: string) {
+    this.router.navigate([url]);
+  }
 }
