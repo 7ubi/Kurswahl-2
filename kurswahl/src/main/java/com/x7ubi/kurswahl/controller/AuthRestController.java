bc17ccfb5af81f7939ffcb0932c8106da89e6f1e
Tobias Renner <tobi.renner03@gmail.com>
2023-10-22 20:08:48 +0200

    feat: Standard Admin for Deployment

--- kurswahl/src/main/java/com/x7ubi/kurswahl/controller/AuthRestController.java
+++ kurswahl/src/main/java/com/x7ubi/kurswahl/controller/AuthRestController.java
@@ -1,20 +1,25 @@
 package com.x7ubi.kurswahl.controller;
 
 import com.x7ubi.kurswahl.jwt.JwtUtils;
+import com.x7ubi.kurswahl.models.Admin;
 import com.x7ubi.kurswahl.models.SecurityUser;
+import com.x7ubi.kurswahl.models.User;
 import com.x7ubi.kurswahl.repository.AdminRepo;
 import com.x7ubi.kurswahl.repository.StudentRepo;
 import com.x7ubi.kurswahl.repository.TeacherRepo;
+import com.x7ubi.kurswahl.repository.UserRepo;
 import com.x7ubi.kurswahl.request.auth.LoginRequest;
 import com.x7ubi.kurswahl.response.common.JwtResponse;
 import com.x7ubi.kurswahl.response.common.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,12 +42,43 @@ public class AuthRestController {
 
     private final StudentRepo studentRepo;
 
-    public AuthRestController(AuthenticationManager authenticationManager, JwtUtils jwtUtils, AdminRepo adminRepo, TeacherRepo teacherRepo, StudentRepo studentRepo) {
+    private final UserRepo userRepo;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${password}")
+    private String standardPassword;
+
+    public AuthRestController(AuthenticationManager authenticationManager, JwtUtils jwtUtils, AdminRepo adminRepo,
+                              TeacherRepo teacherRepo, StudentRepo studentRepo, UserRepo userRepo,
+                              PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
         this.adminRepo = adminRepo;
         this.teacherRepo = teacherRepo;
         this.studentRepo = studentRepo;
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/standardAdmin")
+    public ResponseEntity<?> createStandardAdmin() {
+        logger.info("Creating standard Admin");
+
+        String username = "admin";
+        if (!userRepo.existsByUsername(username)) {
+            Admin admin = new Admin();
+            User user = new User();
+            user.setUsername(username);
+            user.setFirstname("Admin");
+            user.setSurname("Admin");
+            user.setPassword(passwordEncoder.encode(standardPassword));
+            admin.setUser(user);
+
+            userRepo.save(user);
+            adminRepo.save(admin);
+        }
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/login")
