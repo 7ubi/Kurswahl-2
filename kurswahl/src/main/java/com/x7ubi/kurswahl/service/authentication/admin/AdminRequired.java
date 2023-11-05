59bed6fc5bf87266bdafcd59f71a62d0f6506ea3
Tobias Renner <51411393+7ubi@users.noreply.github.com>
2023-11-05 17:12:11 +0100

    feat: @AdminRequired Annotation (#11)

---
+++ kurswahl/src/main/java/com/x7ubi/kurswahl/service/authentication/admin/AdminRequired.java
@@ -0,0 +1,11 @@
+package com.x7ubi.kurswahl.service.authentication.admin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AdminRequired {
+}
