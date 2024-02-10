91ef7378b24d5ce09acf0fd7af14e64a764044d5
Tobias Renner <tobi.renner03@gmail.com>
2024-02-10 16:10:35 +0100

    feat: assign choice returns unfulfilled rules

--- kurswahl/src/main/java/com/x7ubi/kurswahl/admin/choice/response/StudentChoicesResponse.java
+++ kurswahl/src/main/java/com/x7ubi/kurswahl/admin/choice/response/StudentChoicesResponse.java
@@ -1,10 +1,23 @@
 package com.x7ubi.kurswahl.admin.choice.response;
 
+
+import com.x7ubi.kurswahl.common.rule.response.RuleResponse;
+
 import java.util.List;
 
 public class StudentChoicesResponse extends StudentSurveillanceResponse {
 
+    private List<RuleResponse> ruleResponses;
+
     private List<ChoiceResponse> choiceResponses;
+
+    public List<RuleResponse> getRuleResponses() {
+        return ruleResponses;
+    }
+
+    public void setRuleResponses(List<RuleResponse> ruleResponses) {
+        this.ruleResponses = ruleResponses;
+    }
 
     public List<ChoiceResponse> getChoiceResponses() {
         return choiceResponses;
