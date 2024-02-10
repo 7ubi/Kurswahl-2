b73086756d1468d8a8539398e6e57a18268ec169
Tobias Renner <tobi.renner03@gmail.com>
2024-02-10 19:41:00 +0100

    refactor: separate controller in admin choice

---
+++ kurswahl/src/main/java/com/x7ubi/kurswahl/admin/choice/controller/AdminChoiceResultController.java
@@ -0,0 +1,9 @@
+package com.x7ubi.kurswahl.admin.choice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminChoiceResultController {
+}
---
+++ kurswahl/src/main/java/com/x7ubi/kurswahl/admin/choice/service/ChoiceResultService.java
@@ -0,0 +1,7 @@
+package com.x7ubi.kurswahl.admin.choice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChoiceResultService {
+}
