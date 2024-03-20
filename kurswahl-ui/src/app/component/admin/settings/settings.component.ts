dbee6b3ce8783771ae7bf62a0d7f5c87dd932b17
Tobias Renner <tobi.renner03@gmail.com>
2024-03-20 17:43:29 +0100

    feat: settings frontend

---
+++ kurswahl-ui/src/app/component/admin/settings/settings.component.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpService} from "../../../service/http.service";
+import {ClassSizeSettingResponse} from "../admin.responses";
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+  settingsForm: FormGroup;
+  hasChanges: boolean = false;
+
+  classSizeSettingResponse?: ClassSizeSettingResponse;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {
+    this.settingsForm = this.formBuilder.group({
+      warning: ['', [Validators.required, Validators.min(0)]],
+      critical: ['', [Validators.required, Validators.min(0)]]
+    });
+
+    this.httpService.get<ClassSizeSettingResponse>('/api/admin/classSize', response => {
+      this.classSizeSettingResponse = response;
+      this.settingsForm.controls['warning'].setValue(this.classSizeSettingResponse.classSizeWarning);
+      this.settingsForm.controls['critical'].setValue(this.classSizeSettingResponse.classSizeCritical);
+    });
+  }
+
+  onChangeInput() {
+    this.hasChanges = (this.settingsForm.get('warning')?.value !== this.classSizeSettingResponse?.classSizeWarning
+        || this.settingsForm.get('critical')?.value !== this.classSizeSettingResponse?.classSizeCritical)
+      && this.settingsForm.valid;
+  }
+
+  saveSettings() {
+    this.httpService.put<ClassSizeSettingResponse>('/api/admin/classSize', this.getEditClassSizeRequest(),
+      response => this.classSizeSettingResponse = response);
+  }
+
+  private getEditClassSizeRequest() {
+    return {
+      classSizeWarning: this.settingsForm.get('warning')?.value,
+      classSizeCritical: this.settingsForm.get('critical')?.value,
+    };
+  }
+}
