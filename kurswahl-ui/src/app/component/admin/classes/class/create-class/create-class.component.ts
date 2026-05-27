4a362dd6aed1265e5fea089c24c4d652399c1d65
Tobias Renner <tobias.renner@stud.h-da.de>
2026-05-27 09:45:44 +0200

    KW2-110: name automatically subject name, when selected

--- kurswahl-ui/src/app/component/admin/classes/class/create-class/create-class.component.ts
+++ kurswahl-ui/src/app/component/admin/classes/class/create-class/create-class.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {SubjectResponse, TapeResponse, TeacherResponse} from "../../../admin.responses";
 import {HttpService} from "../../../../../service/http.service";
@@ -7,6 +7,7 @@ import {HeroComponent} from "../../../../common/hero/hero.component";
 import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {MatButton} from "@angular/material/button";
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-create-class',
@@ -24,11 +25,12 @@ import {MatButton} from "@angular/material/button";
   ],
   styleUrls: ['./create-class.component.css']
 })
-export class CreateClassComponent implements OnInit {
+export class CreateClassComponent implements OnInit, OnDestroy {
   createClassForm: FormGroup;
   subjectResponses?: SubjectResponse[];
   teacherResponses?: TeacherResponse[];
   tapeResponses!: TapeResponse[];
+  private subjectChangeSub?: Subscription;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -58,6 +60,31 @@ export class CreateClassComponent implements OnInit {
       this.teacherResponses = response;
       this.cdr.detectChanges();
     });
+
+    // When a subject is selected, if the name field is empty, set it to the subject's name
+    const subjectControl = this.createClassForm.get('subject');
+    const nameControl = this.createClassForm.get('name');
+    if (subjectControl && nameControl) {
+      this.subjectChangeSub = subjectControl.valueChanges.subscribe(selectedId => {
+        const currentName = (nameControl.value || '').toString();
+        if (currentName.trim().length > 0) {
+          return; // don't overwrite a user-provided name
+        }
+
+        const subject = this.subjectResponses?.find(s => s.subjectId == selectedId);
+        if (subject) {
+          nameControl.setValue(subject.name);
+          // OnPush change detection: mark for check
+          this.cdr.detectChanges();
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subjectChangeSub) {
+      this.subjectChangeSub.unsubscribe();
+    }
   }
 
   createClass() {
@@ -65,7 +92,7 @@ export class CreateClassComponent implements OnInit {
       return;
     }
 
-    this.httpService.post<undefined>('/api/admin/class', this.getCreateClassRequest(), response => {
+    this.httpService.post<undefined>('/api/admin/class', this.getCreateClassRequest(), () => {
       this.router.navigate(['admin', 'classes']);
     });
   }
@@ -79,7 +106,7 @@ export class CreateClassComponent implements OnInit {
     }
   }
 
-  loadTapes($event: KeyboardEvent) {
+  loadTapes(event: KeyboardEvent) {
     const year = Number((event?.target as HTMLInputElement).value);
 
     this.httpService.get<TapeResponse[]>(`/api/admin/tapes?year=${year}`, response => {
